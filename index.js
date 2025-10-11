47b999fde67d80deaf57128d62515f5c2186ee5b
devofhelloworld <sujaykumarsingh6228@gmail.com>
2025-10-12 01:37:57 +0530

    Test 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {mongoconnect} = require('./utils/databaseutil');
 
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
-app.set('views','views');
+app.set('views',path.join(__dirname,'views'));
 
 
 app.use(homeroutes);
