80e0743a475d1df4abaec6ab2208558ed2b0e18d
devofhelloworld <sujaykumarsingh6228@gmail.com>
2025-10-12 01:57:09 +0530

    Test 4

--- index.js
+++ index.js
@@ -12,24 +12,29 @@ app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 
+async function startserver(){
+  try{
+    await mongoconnect();
+    console.log('MongoDB Connection Succesfull!');
+    app.use(homeroutes);
+    app.use(express.urlencoded());
+    app.use(foundrouter);
+    app.use(lostrouter);
+    app.use(claimrequestrouter);
+    app.use(searchrouter);
 
-app.use(homeroutes);
-app.use(express.urlencoded());
-app.use(foundrouter);
-app.use(lostrouter);
-app.use(claimrequestrouter);
-app.use(searchrouter);
-
-app.get('/contact',(req,res,next)=>{
-  res.render('contact',{pagetitle:'Contact Me!'});
-})
-app.use((req,res,next)=>{
-  res.render('404',{pagetitle:'Page not found!'});
-})
+    app.get('/contact',(req,res,next)=>{
+      res.render('contact',{pagetitle:'Contact Me!'});
+    })
+    app.use((req,res,next)=>{
+      res.render('404',{pagetitle:'Page not found!'});
+    })
+  } catch(err){
+    console.error('Failed to connect to mongodb: ',err);
+  }
+}
 
 
-mongoconnect(()=>{
-  console.log('MongoDB Connection Succesfull!');
-});
+startserver();
 
 module.exports = app;
